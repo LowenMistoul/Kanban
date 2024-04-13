1f10dd1528a9e5347515350ba17f804ade5219ab
Lowen Mistoul <mistouldlowen@gmail.com>
2024-04-13 00:07:00 -0400

    builder for usertype modification

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -18,13 +18,24 @@ class UserType extends AbstractType
     {
         $builder
             ->add('userName')
-            ->add('email',EmailType::class)
+            ->add('email',EmailType::class);
+
+        if($this->getUser()){
+            $builder
+                ->add('password', RepeatedType::class, array(
+                    'type' => PasswordType::class,
+                    'first_options'  => array('label' => 'Password'),
+                    'second_options' => array('label' => 'Repeat Password'),
+                    'required' => false
+                ));
+        }else{
+            $builder
             ->add('password', RepeatedType::class, array(
                 'type' => PasswordType::class,
                 'first_options'  => array('label' => 'Password'),
                 'second_options' => array('label' => 'Repeat Password'),
-            ))
-        ;
+            ));
+        }
     }
 
     public function configureOptions(OptionsResolver $resolver): void
