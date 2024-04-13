ef8e22871a5a67b00c6db5e8bfc28e293c62f37e
Lowen Mistoul <mistouldlowen@gmail.com>
2024-04-12 22:09:35 -0400

    trying profile

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -47,4 +47,40 @@ class RegistrationController extends AbstractController
             array('form' => $form->createView(),'title'=>'Sign up')
         );
     }
+
+        /**
+     * @Route("/update", name="user_update")
+     */
+    public function update(Request $request, UserPasswordHasherInterface $passwordEncoder,EntityManagerInterface $manager)
+    {
+        // 1) build the form
+        $user = $this->getuser();
+        $form = $this->createForm(UpdateType::class, $user);
+
+        // 2) handle the submit (will only happen on POST)
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            // 3) Encode the password (you could also do this via Doctrine listener)
+            $password = $passwordEncoder->hashPassword($user, $user->getPassword());
+            $user->setPassword($password);
+
+            // 4) save the User!
+            //$entityManager = $this->getDoctrine()->getManager();
+            // $entityManager->persist($user);
+            // $entityManager->flush();
+            $persist = $manager->persist($user);
+            $flush = $manager->flush();
+
+            // ... do any other work - like sending them an email, etc
+            // maybe set a "flash" success message for the user
+
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render(
+            'security/update.html.twig',
+            array('form' => $form->createView(),'title'=>'Information')
+        );
+    }
 }
