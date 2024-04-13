8059907c8b94d31c7494abaeb29b634288820de1
Lowen Mistoul <mistouldlowen@gmail.com>
2024-04-12 20:57:55 -0400

    adding profile page

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -46,4 +46,40 @@ class RegistrationController extends AbstractController
             array('form' => $form->createView(),'title'=>'Sign up')
         );
     }
+
+    /**
+     * @Route("/updateUser", name="user_update")
+     */
+    public function updateUser(Request $request, UserPasswordHasherInterface $passwordEncoder,EntityManagerInterface $manager)
+    {
+        // 1) build the form
+        $user = $this->getUser();
+        $form = $this->createForm(UserType::class, $user);
+
+        // 2) handle the submit (will only happen on POST)
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            // 3) Encode the password (you could also do this via Doctrine listener)
+            $password = $passwordEncoder->hashPassword($user, $user->getPassword());
+            $user->setPassword($password);
+
+            // 4) save the User!
+            //$entityManager = $this->getDoctrine()->getManager();
+            // $entityManager->persist($user);
+            // $entityManager->flush();
+            $persist = $manager->persist($user);
+            $flush = $manager->flush();
+
+            // ... do any other work - like sending them an email, etc
+            // maybe set a "flash" success message for the user
+
+            return $this->redirectToRoute('updateUser');
+        }
+
+        return $this->render(
+            'security/updateUser.html.twig',
+            array('form' => $form->createView(),'title'=>'Sign up')
+        );
+    }
 }
