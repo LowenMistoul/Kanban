1f99709f014f581e5f210a0a3ecbb8f9e1296e9f
Lowen Mistoul <mistouldlowen@gmail.com>
2024-04-15 09:41:56 -0400

    completely removing password from profile

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -55,7 +55,7 @@ class RegistrationController extends AbstractController
         // 1) build the form
         $user = $this->getUser();
         $form = $this->createForm(UserType::class, $user);
-
+        $form->remove('password');
         // 2) handle the submit (will only happen on POST)
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
