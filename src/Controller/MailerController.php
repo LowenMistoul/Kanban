4fad10793ec16092541ca0dadf6da23ac3d0ad4f
Lowen Mistoul <mistouldlowen@gmail.com>
2024-04-18 09:06:56 -0400

    test

--- src/Controller/MailerController.php
+++ src/Controller/MailerController.php
@@ -7,26 +7,25 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Email;
 use Symfony\Component\Mime\Address;
-use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 
 class MailerController extends AbstractController
 {
     #[Route('/mailer-send', name: 'app_mailer', methods: ['GET', 'POST'])]
     public function sendEmail(MailerInterface $mailer):Response
     {
-        $email = (new Email())
-        ->from(new Address('mailtrap@demomailtrap.com',"Mailtrap"))
-        ->to('mistouldlowen@gmail.com')
-        //->cc('cc@example.com')
-        //->bcc('bcc@example.com')
-        //->replyTo('fabien@example.com')
-        //->priority(Email::PRIORITY_HIGH)
-        ->subject('Time for Symfony Mailer!')
-       // ->text('Sending emails is fun again!')
-        ->html('<p>See Twig integration for better HTML integration!</p>');
+//         $email = (new Email())
+//         ->from(new Address('mailtrap@demomailtrap.com',"Mailtrap"))
+//         ->to('mistouldlowen@gmail.com')
+//         //->cc('cc@example.com')
+//         //->bcc('bcc@example.com')
+//         //->replyTo('fabien@example.com')
+//         //->priority(Email::PRIORITY_HIGH)
+//         ->subject('Time for Symfony Mailer!')
+//        // ->text('Sending emails is fun again!')
+//         ->html('<p>See Twig integration for better HTML integration!</p>');
 
-//....
-  $mailer->send($email);
+// //....
+//   $mailer->send($email);
 
         // ...
       return new Response(
