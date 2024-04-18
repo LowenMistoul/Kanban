3a1e4a3e4755041c1c1dbfe83c167d64deca0b0f
Lowen Mistoul <mistouldlowen@gmail.com>
2024-04-18 09:10:36 -0400

    test

--- src/Controller/MailerController.php
+++ src/Controller/MailerController.php
@@ -13,16 +13,16 @@ class MailerController extends AbstractController
     #[Route('/mailer-send', name: 'app_mailer', methods: ['GET', 'POST'])]
     public function sendEmail(MailerInterface $mailer):Response
     {
-//         $email = (new Email())
-//         ->from(new Address('mailtrap@demomailtrap.com',"Mailtrap"))
-//         ->to('mistouldlowen@gmail.com')
+        $email = (new Email())
+        ->from(new Address('mailtrap@demomailtrap.com',"Mailtrap"))
+        ->to('mistouldlowen@gmail.com')
 //         //->cc('cc@example.com')
 //         //->bcc('bcc@example.com')
 //         //->replyTo('fabien@example.com')
 //         //->priority(Email::PRIORITY_HIGH)
-//         ->subject('Time for Symfony Mailer!')
+        ->subject('Time for Symfony Mailer!')
 //        // ->text('Sending emails is fun again!')
-//         ->html('<p>See Twig integration for better HTML integration!</p>');
+        ->html('<p>See Twig integration for better HTML integration!</p>');
 
 // //....
 //   $mailer->send($email);
