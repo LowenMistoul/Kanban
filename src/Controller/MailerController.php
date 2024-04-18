107666d69a5c0528c4ef3375ebfd02fda15b634a
Lowen Mistoul <mistouldlowen@gmail.com>
2024-04-18 07:57:12 -0400

    test

--- src/Controller/MailerController.php
+++ src/Controller/MailerController.php
@@ -6,15 +6,16 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Email;
+use Symfony\Component\Mime\Address;
 use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 
 class MailerController extends AbstractController
 {
     #[Route('/mailer', name: 'app_mailer')]
-    public function sendEmail(MailerInterface $mailer)
+    public function sendEmail(MailerInterface $mailer):Response
     {
         $email = (new Email())
-        ->from('mailtrap@demomailtrap.com')
+        ->from(new Address('mailtrap@demomailtrap.com',"Mailtrap"))
         ->to('mistouldlowen@gmail.com')
         //->cc('cc@example.com')
         //->bcc('bcc@example.com')
