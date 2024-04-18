8b0d7c0785849c264c316f78e11600c5df634a39
Lowen Mistoul <mistouldlowen@gmail.com>
2024-04-17 20:30:00 -0400

    security for forms

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -52,30 +52,34 @@ class RegistrationController extends AbstractController
      */
     public function update(Request $request, UserPasswordHasherInterface $passwordEncoder,EntityManagerInterface $manager)
     {
-        // 1) build the form
-        $user = $this->getUser();
-        $form = $this->createForm(UserType::class, $user);
-        $form->remove('password');
-        // 2) handle the submit (will only happen on POST)
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            // 3) save the User!
-            // $entityManager = $this->getDoctrine()->getManager();
-            // $entityManager->persist($user);
-            // $entityManager->flush();
-            $persist = $manager->persist($user);
-            $flush = $manager->flush();
+        if($this->getUser()){
+            // 1) build the form
+            $user = $this->getUser();
+            $form = $this->createForm(UserType::class, $user);
+            $form->remove('password');
+            // 2) handle the submit (will only happen on POST)
+            $form->handleRequest($request);
+            if ($form->isSubmitted() && $form->isValid()) {
+                // 3) save the User!
+                // $entityManager = $this->getDoctrine()->getManager();
+                // $entityManager->persist($user);
+                // $entityManager->flush();
+                $persist = $manager->persist($user);
+                $flush = $manager->flush();
 
-            // ... do any other work - like sending them an email, etc
-            // maybe set a "flash" success message for the user
+                // ... do any other work - like sending them an email, etc
+                // maybe set a "flash" success message for the user
 
-            return $this->redirectToRoute('user_update');
+                return $this->redirectToRoute('user_update');
+            }
+
+            return $this->render(
+                'security/update.html.twig',
+                array('form' => $form->createView())
+            );
+        }else {
+            return $this->render('security/acces.html.twig');
         }
-
-        return $this->render(
-            'security/update.html.twig',
-            array('form' => $form->createView())
-        );
     }
 
     /**
@@ -83,33 +87,37 @@ class RegistrationController extends AbstractController
      */
     public function updatePassword(Request $request, UserPasswordHasherInterface $passwordEncoder,EntityManagerInterface $manager)
     {
-        // 1) build the form
-        $user = $this->getUser();
-        $form = $this->createForm(UserType::class, $user);
-        $form->remove('email');
-        $form->remove('userName');
-        // 2) handle the submit (will only happen on POST)
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            // 3) Encode the password (you could also do this via Doctrine listener)
-            $password = $passwordEncoder->hashPassword($user, $user->getPassword());
-            $user->setPassword($password);
-            // 4) save the User!
-            // $entityManager = $this->getDoctrine()->getManager();
-            // $entityManager->persist($user);
-            // $entityManager->flush();
-            $persist = $manager->persist($user);
-            $flush = $manager->flush();
+        if($this->getUser()){
+            // 1) build the form
+            $user = $this->getUser();
+            $form = $this->createForm(UserType::class, $user);
+            $form->remove('email');
+            $form->remove('userName');
+            // 2) handle the submit (will only happen on POST)
+            $form->handleRequest($request);
+            if ($form->isSubmitted() && $form->isValid()) {
+                // 3) Encode the password (you could also do this via Doctrine listener)
+                $password = $passwordEncoder->hashPassword($user, $user->getPassword());
+                $user->setPassword($password);
+                // 4) save the User!
+                // $entityManager = $this->getDoctrine()->getManager();
+                // $entityManager->persist($user);
+                // $entityManager->flush();
+                $persist = $manager->persist($user);
+                $flush = $manager->flush();
 
-            // ... do any other work - like sending them an email, etc
-            // maybe set a "flash" success message for the user
+                // ... do any other work - like sending them an email, etc
+                // maybe set a "flash" success message for the user
 
-            return $this->redirectToRoute('user_update_password');
+                return $this->redirectToRoute('user_update_password');
+            }
+
+            return $this->render(
+                'security/password.html.twig',
+                array('form' => $form->createView())
+            );
+        }else {
+            return $this->render('security/acces.html.twig');
         }
-
-        return $this->render(
-            'security/password.html.twig',
-            array('form' => $form->createView())
-        );
     }
 }
